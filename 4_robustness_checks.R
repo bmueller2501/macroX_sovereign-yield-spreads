09c69e5cb3a065e12c944c967014345b7557e378
Gutschlhofer <jakob.gutschlhofer@wu.ac.at>
2020-09-17 11:01:05 +0200

    Add structure for robustness checks

---
+++ 4_robustness_checks.R
@@ -0,0 +1,15 @@
+# robustness checks
+## 1. replication steps of the paper to check similarities/differences
+## 2. actual robustness checks
+
+## 1. replication steps of the paper to check similarities/differences
+
+### 1.1 replication of BMAs in table 2
+
+#### 1.1.1 baseline (1999-2009)
+#### 1.1.2 lagged (1999-2009)
+#### 1.1.3 pre-crisis (1999-2007)
+
+## 2. actual robustness checks
+
+### 2.1 unmodified variables (not relative to Germany)
