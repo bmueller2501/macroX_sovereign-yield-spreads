98548324ec3ae27f27145d3b47f04990a5042590
Gutschlhofer <jakob.gutschlhofer@wu.ac.at>
2020-09-17 13:23:17 +0200

    Add analysis, lagged vars, and setup_model function

--- code/3_merge_model.R
+++ code/3_merge_model.R
@@ -22,14 +22,22 @@ rm(deu)
 
 # model, first try with BMS pack and data relative to Germany
 
+setup_model <- function(data_rel){
+  return(dat_rel %>% 
+           filter(iso3 != "DEU") %>% 
+           select(spr, vars) %>%
+           select(-c(pspp,
+                     i_us,
+                     baa,
+                     gdp)))
+}
+
 moddat <- dat_rel %>% 
-  filter(year %in% c(1999:2019), iso3 != "DEU") %>% 
-  select(spr, vars) %>%
-  select(-c(pspp, # @Benjamin: error if i include the new measure of PSPP? why?
-            i_us,
-            baa,
-            gdp))
+  filter(year %in% c(1999:2019)) %>% 
+  setup_model()
 
 moddat <- as.matrix(moddat)
 
 mod.BMS <- bms(moddat, burn = 100000, iter = 200000, g = "BRIC", mprior = "uniform", mcmc = "bd"); summary(mod.BMS)
+
+coef(mod.BMS, exact = TRUE) # exact=TRUE: This means that the marginal likelihoods are used for BMA
