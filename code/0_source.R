ff9e45e1c4951544c4dad5b1093d2e580109ac5b
bmueller2501 <b.mueller2501@gmail.com>
2020-09-23 11:10:52 +0200

    created yearly average and end-of-year graphs

--- code/0_source.R
+++ code/0_source.R
@@ -15,8 +15,8 @@ pacman::p_load(BMS)
 pacman::p_load(fastDummies)
 
 # set settings for this run
-joined_1999 <- TRUE # defines whether to only consider the subset of countries that joined the EMU 1999
-j99 <- c("AUT", "BEL", "DEU", "ESP", "FIN", "FRA", "ITA", "NLD", "PRT", "IRL", "LUX")
+j99 <- TRUE # defines whether to only consider the subset of countries that joined the EMU 1999
+c_j99 <- c("AUT", "BEL", "DEU", "ESP", "FIN", "FRA", "ITA", "NLD", "PRT", "IRL", "LUX")
 
 # setwd("C:/Users/Viktor/Dropbox/GitHub/macroX_sovereign-yield-spreads/")
 # setwd("C:/Users/bmuel/Desktop/GitHub/macroX_sovereign-yield-spreads")
--- code/1_dependent_var.R
+++ code/1_dependent_var.R
@@ -1,4 +1,7 @@
-# load data for the dependent variable (spreads)
+
+##-------------------------------------------------------------------------------------------------
+## load data for the dependent variable (spreads) and create average year and end-of-year
+## data set for spreads
 
 # load data
 dat <- readxl::read_xls("./input/spreads.xls")
@@ -6,8 +9,8 @@ c <- colnames(dat[,2:ncol(dat)])
 
 # get year from date
 dat <- dat %>% 
-  mutate(year = as.integer(substr(as.character(dat$date), 1, 4))) %>% 
-  select(-date)
+  mutate(year = as.integer(substr(as.character(dat$date), 1, 4))) #%>% 
+  #select(-date)
 
 # adjust table to get: year, iso3, spread
 dat <- gather(data = dat, key = "iso3", value = "spr", c) %>%
@@ -20,47 +23,51 @@ dat$iso3[dat$iso3 == "IRN"] <- "IRL"
 # filter out entries with missing dates
 dat <- dat %>% filter(!is.na(year)); summary(dat)
 
-# get the average spread for each country for each year
-dat <- dat[, .(spr = mean(spr, na.rm = T)), by = .(year, iso3)]
-
-dep <- data.table(dat)
-rm(dat)
-
-# filter for countries that joined the EMU in 1999 in case setting is active
-if(joined_1999){
-  dep <- dep %>% filter(iso3 %in% j99)
-}
-
-
-############################################
-# change dep variable to end of year value #
-
-dat <- readxl::read_xls("./input/spreads.xls")
-c <- colnames(dat[,2:ncol(dat)])
-
-# adjust table to get: year, iso3, spread
-dat <- gather(data = dat, key = "iso3", value = "spr", c) %>%
-  mutate(iso3 = countrycode(iso3, "iso2c", "iso3c")) %>%
-  as.data.table
-
-# get year from date, get last value for each year
-dat <- dat %>%
-  mutate(year = as.integer(substr(as.character(dat$date), 1, 4))) %>% 
+# get the end-of-year value
+dep_ey <- dat %>%
   group_by(iso3, year) %>% 
   filter(date == max(date)) %>% 
   select(-date) %>% 
+  rename(spr_ey = "spr") %>%
   ungroup()
 
-# fix code for Ireland
-dat$iso3[dat$iso3 == "IRN"] <- "IRL"
-
-# filter out entries with missing dates
-dat <- dat %>% filter(!is.na(year)); summary(dat)
-
-dep_last <- data.table(dat)
-rm(dat)
+# get the average spread for each country for each year
+dep <- dat[, .(spr = mean(spr, na.rm = T)), by = .(year, iso3)]
 
 # filter for countries that joined the EMU in 1999 in case setting is active
-if(joined_1999){
-  dep_last <- dep_last %>% filter(iso3 %in% j99)
+if(j99){
+  dep <- dep %>% filter(iso3 %in% c_j99)
+  dep_ey <- dep_ey %>% filter(iso3 %in% c_j99)
 }
+
+rm(dat)
+
+
+##-------------------------------------------------------------------------------------------------
+## plot yearly average spreads and difference between average and end-of-year
+
+plotdat <- merge(dep, dep_ey) %>%
+  filter(year <= 2019, iso3 != "LUX") %>% #only 1999-2019 and without LUX
+  mutate(diff = spr-spr_ey)
+
+y <- seq(1999, 2019, by = 2)
+
+# yearly average spreads
+ggplot(plotdat) +
+  geom_line(aes(x=year, y=spr, color=iso3)) + 
+  scale_x_continuous(labels = as.character(y), breaks = y) +
+  labs(x='year', y="yearly average spread", 
+       color = "countries (iso3)")
+ggsave("output/time.spr_avg.png", width=8, height=5)
+
+
+# difference between average spread of the year and spread at the end of the year
+ggplot(plotdat) +
+  geom_line(aes(x=year, y=diff, color=iso3)) + 
+  scale_x_continuous(labels = as.character(y), breaks = y) +
+  labs(x='year', y="difference between yearly average and end-of-year spread", 
+       color = "countries (iso3)")
+ggsave("output/time.spr_diff.png", width=8, height=5)
+
+
+
