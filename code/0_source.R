8f696b532f3af1c6edae1ab3b676a1d19d225b8c
bmueller2501 <b.mueller2501@gmail.com>
2020-09-23 11:35:59 +0200

    1 and 2 updated

--- code/0_source.R
+++ code/0_source.R
@@ -16,14 +16,14 @@ pacman::p_load(fastDummies)
 
 # set settings for this run
 j99 <- TRUE # defines whether to only consider the subset of countries that joined the EMU 1999
-c_j99 <- c("AUT", "BEL", "DEU", "ESP", "FIN", "FRA", "ITA", "NLD", "PRT", "IRL", "LUX")
+cj99 <- c("AUT", "BEL", "DEU", "ESP", "FIN", "FRA", "ITA", "NLD", "PRT", "IRL", "LUX")
 
 # setwd("C:/Users/Viktor/Dropbox/GitHub/macroX_sovereign-yield-spreads/")
 # setwd("C:/Users/bmuel/Desktop/GitHub/macroX_sovereign-yield-spreads")
 
-source(paste("./code/1_dependent_var.R", sep =""))
-source(paste("./code/2_explanatory_vars.R", sep =""))
-source(paste("./code/3_merge_model.R", sep =""))
-source(paste("./code/4_robustness_checks.R", sep =""))
-source(paste("./code/5_analysis.R", sep =""))
-source(paste("./code/6_ssvs.R", sep =""))
+# source(paste("./code/1_dependent_var.R", sep =""))
+# source(paste("./code/2_explanatory_vars.R", sep =""))
+# source(paste("./code/3_merge_model.R", sep =""))
+# source(paste("./code/4_robustness_checks.R", sep =""))
+# source(paste("./code/5_analysis.R", sep =""))
+# source(paste("./code/6_ssvs.R", sep =""))
