cf9215cd12b61f53fdf5efccca5069e7edad6adb
bmueller2501 <b.mueller2501@gmail.com>
2020-09-29 22:19:23 +0200

    use tot instead of tot growth, added data frames for extended var

--- code/3_merge_model.R
+++ code/3_merge_model.R
@@ -2,19 +2,21 @@
 ##-------------------------------------------------------------------------------------------------
 ## merge the dep and exp into model data sets and omit NAs
 
-dat_base <- merge(dep, exp, by = c("year", "iso3")) %>% na.omit
+dat <- merge(dep, exp, by = c("year", "iso3")) %>% na.omit
 dat_ey <- merge(dep_ey, exp_ey, by = c("year", "iso3")) %>% na.omit
 dat_lag <- merge(dep, exp_lag, by = c("year", "iso3")) %>% na.omit
 
 dat_ey_notrel <- merge(dep_ey, exp_ey_notrel, by = c("year", "iso3")) %>% na.omit
 
-#we have 13 NAs in the spread varaible from LUX in the early years
+dat_ext <- merge(dep, exp_ext, by = c("year", "iso3")) %>% na.omit
+dat_ey_ext <- merge(dep_ey, exp_ey_ext, by = c("year", "iso3")) %>% na.omit
 
 
 ##-------------------------------------------------------------------------------------------------
 ## functions for setup and run
 
 setup_model <- function(data){
+  vars <- names(data)[!names(data) %in% c("year", "iso3")]
   data %>% 
     filter(iso3 != "DEU") %>% 
     fastDummies::dummy_cols(select_columns = c("iso3")) %>%
@@ -23,7 +25,7 @@ setup_model <- function(data){
 }
 
 run_model <- function(data){
-  bms(as.matrix(data), burn = 5000, iter = 30000, g = "BRIC", mprior = "uniform", mcmc = "rev.jump")
+  bms(as.matrix(data), burn = 5000, iter = 20000, g = "BRIC", mprior = "uniform", mcmc = "rev.jump")
 }
 
 
@@ -33,7 +35,7 @@ run_model <- function(data){
 variables.bma <- data.frame(
   No = 1:14,
   code = c("bb", "debt_ratio", "i_avg", "g", "inf", "inf_var", "gcf", "tb", "openness",
-           "tot_g", "debt", "debt_ea", "i_us", "bbb"),
+           "tot_oecd", "debt", "debt_ea", "i_us", "bbb"),
   Variable = c(
     # Country specific yield spread drivers
     "Budget balance to GDP",
@@ -45,7 +47,7 @@ variables.bma <- data.frame(
     "Capital formation",
     "Trade balance",
     "Openness",
-    "Terms of Trade growth",
+    "Terms of Trade",
     # Liquidity indicators
     "Total government debt",
     "Total government debt to total government debt of all EMU countries",
@@ -54,16 +56,46 @@ variables.bma <- data.frame(
     "Market sentiment: BBB rated US corporate bond spread to US treasuries")
 )
 
+variables.bma.ext <- data.frame(
+  No = 1:17,
+  code = c("bb", "debt_ratio", "i_avg", "g", "inf", "inf_var", "gcf", "tb", "openness",
+           "tot_oecd", "debt", "debt_ea", "i_us", "bbb", "pensions", "reer","pspp"),
+  Variable = c(
+    # Country specific yield spread drivers
+    "Budget balance to GDP",
+    "Total government debt to GDP",
+    "Average interest rate",
+    "GDP growth",
+    "Inflation",
+    "Inflation variation",
+    "Capital formation",
+    "Trade balance",
+    "Openness",
+    "Terms of Trade",
+    # Liquidity indicators
+    "Total government debt",
+    "Total government debt to total government debt of all EMU countries",
+    # Global conditions
+    "Global capital costs: US interest rate",
+    "Market sentiment: BBB rated US corporate bond spread to US treasuries",
+    # Extended variables
+    "Pension expenditure",
+    "Real effective exchange rate",
+    "Public Sector Purchase Programme"
+    )
+)
 
-model_results_as_table <- function(model){
+
+model_results_as_table <- function(model, ext){
   cf <- as.data.frame(coef(model, exact = TRUE))
   cf$code <- row.names(cf)
+  cf <- cf %>% filter(!grepl("iso3", code))
   
-  cf <- cf %>% 
-    filter(!grepl("iso3", code)) %>% 
-    # join variables.bma to get naming and ordering right
-    left_join(variables.bma, by = c("code")) %>% 
-    # prepare for output
+  if(ext == T) {cf <- cf %>% left_join(variables.bma.ext, by = c("code"))} else{
+    cf <- cf %>% left_join(variables.bma, by = c("code"))
+  }
+  
+  cf <- cf %>%
     mutate(
       PI = round(PIP*100, 1),
       DI = if_else(Cond.Pos.Sign == 0.5, "+/-",
